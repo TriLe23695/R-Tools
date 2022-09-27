1ea60189988629614f34e15851744b8f3535de5c
TriLe23695 <106644531+TriLe23695@users.noreply.github.com>
2022-09-27 18:24:50 +0700

    first commit

--- Day03/Day03.R
+++ Day03/Day03.R
@@ -3,3 +3,20 @@ df <- read.csv("D:\\Data Analysis\\R-Tools\\Day03\\airtravel.csv")
 
 print(df)
 
+#In ra số cột trong df
+cat("Total Columns",ncol(df))
+
+#In ra số dòng trong df
+cat("Total rows",nrow(df))
+
+#Số lượng người đi du lịch ít nhất năm 1960
+min_1960s <- min(df$X1960)
+
+min_1960s
+
+#Số lượng người đi du lịch lớn nhất năm 1960
+max_1960s <- max(df$X1960)
+
+max_1960s
+
+#Lấy những dòng dữ liệu
